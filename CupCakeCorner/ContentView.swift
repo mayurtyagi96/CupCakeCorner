621157d09e879a5083c7a0f261eabbaea4290e26
mayurtyagi <mayurtyagi@unolo>
2024-08-06 00:25:58 +0530

    1st lession

--- CupCakeCorner/ContentView.swift
+++ CupCakeCorner/ContentView.swift
@@ -7,15 +7,47 @@
 
 import SwiftUI
 
+struct Response: Codable{
+    var results: [Result]
+}
+
+struct Result: Codable{
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+
 struct ContentView: View {
+    @State private var results = [Result]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.trackId){ result in
+            VStack(alignment: .leading){
+                Text(result.trackName)
+                    .font(.headline)
+                
+                Text(result.collectionName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async{
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid Url")
+            return
+        }
+        do{
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedData = try? JSONDecoder().decode(Response.self, from: data){
+                results = decodedData.results
+            }
+        }catch{
+            print("Not getting the data")
+        }
+        
     }
 }
 
